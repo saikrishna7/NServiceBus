cbbba30eb79a0c60dfc28a6142a603973ca72520
saikrishna7 <saikrishna7@live.com>
2018-12-30 16:33:51 -0600

    Add project files.

---
+++ Sales/PlaceOrderHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Messages;
+using NServiceBus;
+using NServiceBus.Logging;
+
+namespace Sales
+{
+    public class PlaceOrderHandler :
+        IHandleMessages<PlaceOrder>
+    {
+        static Random random = new Random();
+
+        
+
+        static ILog log = LogManager.GetLogger<PlaceOrderHandler>();
+
+        #region UpdatedHandler
+
+        public Task Handle(PlaceOrder message, IMessageHandlerContext context)
+        {
+            log.Info($"Received PlaceOrder, OrderId = {message.OrderId}");
+
+            if (random.Next(0, 5) == 0)
+            {
+                throw new Exception("Oops");
+            }
+
+            // This is normally where some business logic would occur
+
+            var orderPlaced = new OrderPlaced
+            {
+                OrderId = message.OrderId
+            };
+            return context.Publish(orderPlaced);
+        }
+
+        #endregion
+    }
+}
